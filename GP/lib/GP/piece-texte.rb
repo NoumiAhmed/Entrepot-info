ac4029f0d53628773d5290d4430d9342a39647e3
Ahmed Noumi <ac891148@malt.labunix.uqam.ca>
2017-05-01 18:11:36 -0400

    rajout du fichier piece-texte

---
+++ GP/lib/GP/piece-texte.rb
@@ -0,0 +1,31 @@
+module GP
+
+
+# Module qui specifi les details du format
+#  textuel pour la base de donnees,
+#
+#
+#
+  module PiecesTexte
+# Separateur pour les champs d'un enregistrement specifiant une piece.
+#
+  SEPARATEUR = '|'
+  SEP = SEPARATEUR
+
+# methode pour creer un objet piece a partir d'une ligne lue dans le
+# depot de donnees.
+#
+  def self.creer_piece( ligne )
+    noSerie, type, description = ligne.chomp.split(SEP)
+    Piece.new( noSerie.to_sym, type, description )
+  end
+
+# Methode pour sauvegarder un objet piece dans le depot de donnees.
+#
+  def self.sauver_cours( fich, piece )
+
+   fich.puts [piece.noSerie, piece.type, piece.description].join(PiecesTexte::SEP)
+  end
+
+ end
+end
