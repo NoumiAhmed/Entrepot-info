2b6cde13506edcc68a48b494da56d10556fb9279
turkguy <turkguy@hotmail.com>
2017-05-02 21:53:30 -0400

    implementation d'un test

--- GP/test/test_helper.rb
+++ GP/test/test_helper.rb
@@ -1,9 +1,27 @@
-require 'test/unit'
+gem 'minitest'
+require 'minitest/autorun'
+require 'minitest/spec'
+require 'minitest/mock'
+require 'open3'
 
 # Add test libraries you want to use here, e.g. mocha
 
-class Test::Unit::TestCase
+class Object
+	# Pour desactiver temporairement une suite de tests.
+  def _describe( test )
+    puts "--- On saute les tests pour \"#{test}\" ---"
+  end
 
-  # Add global extensions to the test case class here
+  # Pour desactiver temporairement un test.
+  def _it( test )
+    puts "--- On saute les tests pour \"#{test}\" ---"
+  end
+
+  
+
+	# # Des alias pour style RSpec
+  alias_method :context, :describe
+  alias_method :_context, :_describe
+  #Add global extensions to the test case class here
   
 end
